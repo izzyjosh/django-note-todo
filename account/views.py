3b68bf7bef9bc1e44fc665051dfbb30c57d51601
joshua <joshuajosephizzyjosh@gmail.com>
2023-08-08 23:50:20 +0100

    changes

--- account/views.py
+++ account/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.models import User,auth
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from django.contrib.auth.decorators import @login_required
+from django.contrib.auth.decorators import @ login_required
 
 
-#function that handle the signup process
+# function that handle the signup process
 def signup(request):
     if request.method == "POST":
-        username = request.POST,get("username")
+        username = request.POST, get("username")
         email = request.POST.get("email")
         password = request.POST.get("password")
         confirm_password = request.POST.get("confirm_password")
@@ -20,33 +20,34 @@ def signup(request):
                         email=email,
                         password=password
                     )
-                    messages.info(request,"successfully created account")
+                    messages.info(request, "successfully created account")
                     return redirect("signin")
                 else:
-                    messages.error(request,"both password do not correspond ")
+                    messages.error(request, "both password do not correspond ")
             else:
-                messages.error(request,"email already exist")
+                messages.error(request, "email already exist")
         else:
-            messages.error(request,"the username already exist")
+            messages.error(request, "the username already exist")
     return render(request, "signup.html")
 
-#function that handles the signin
+
+# function that handles the signin
 def signin(request):
     if request.method == "POST":
-        username = request.POST.get("username")   
-        password = request.POST.get("password") 
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
-        user = auth.authenticate(username=username,password=password)
+        user = auth.authenticate(username=username, password=password)
         if user is not None:
-            auth.login(request,user)
+            auth.login(request, user)
             return redirect("#")
         else:
             messages.error(request, "invalid email or password")
-    return render(request,"signin.html")
+    return render(request, "signin.html")
 
 
-#function that logs out user
+# function that logs out user
 @login_required
-def logout(request): 
+def logout(request):
     auth.logout(request)
-    return redirect("signin") 
+    return redirect("signin")
