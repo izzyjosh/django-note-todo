cfceec717da424c7790caa9f53c9e4cbf48b5cb4
joshua <joshuajosephizzyjosh@gmail.com>
2023-08-12 21:52:54 +0100

    added my notepad

--- notepad/models.py
+++ notepad/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.model):
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+
+
+class Note(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    category = models.CharField(Category, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
