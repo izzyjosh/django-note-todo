91f0f033db490a44d3be1f48ef74988877c49014
izzyjosh <calebayuba222@gmail.com>
2023-08-16 23:22:57 +0100

    made some changes

--- notepad/views.py
+++ notepad/views.py
@@ -7,14 +7,14 @@ def index(request):
     search = request.GET.get("search", "")
     cat = request.GET.get("category", "")
 
-    note = Note.objects.filter(
+    notes = Note.objects.filter(
         Q(title__icontains=search) |
         Q(body__icontains=search) |
         Q(category__name=cat),
         owner=request.user
     )
     context = {
-        "note": note
+        "notes": notes
     }
 
     return render(request, "index.html", context)
